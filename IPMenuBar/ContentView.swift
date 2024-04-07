70a99d2bf899c81c39368e9c6a04725e348f34da
Jan Baraniewski <dev@baraniewski.com>
2024-04-07 22:37:32 +0200

    Initial Commit

---
+++ IPMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IPMenuBar
+//
+//  Created by Jan Baraniewski on 07/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IPMenuBar/IPMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPMenuBarApp.swift
+//  IPMenuBar
+//
+//  Created by Jan Baraniewski on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct IPMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
